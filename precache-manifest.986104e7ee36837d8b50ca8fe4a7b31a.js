8dad0e8f0e2829b1d74121c0282dbe53452040dc
Mayank Mittal <mayank.mittal@allocatesoftware.com>
2020-04-24 20:13:46 +0530

    Updates

---
+++ precache-manifest.986104e7ee36837d8b50ca8fe4a7b31a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43ecfa1aa63224367c786ed10f5e884",
+    "url": "/Notes-List/index.html"
+  },
+  {
+    "revision": "9615e1d53079ec867984",
+    "url": "/Notes-List/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7204a21a3aadb3c76948",
+    "url": "/Notes-List/static/js/2.1e49cae4.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/Notes-List/static/js/2.1e49cae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9615e1d53079ec867984",
+    "url": "/Notes-List/static/js/main.3be918ed.chunk.js"
+  },
+  {
+    "revision": "8b281fcf7bb0b1f29c76",
+    "url": "/Notes-List/static/js/runtime-main.348c1863.js"
+  }
+]);
