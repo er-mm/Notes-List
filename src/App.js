a5a665bd8e26005a83eecf11e09716c701131392
Mayank Mittal <mayank.mittal@allocatesoftware.com>
2020-04-25 15:15:26 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LearningRedux } from "./components/LearningRedux";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 
 class App extends React.Component {
